af84a203cd624ef3f76c72554a202345c1c59e18
NasifaTalukder <nasifatldr36@gmail.com>
2023-05-26 13:23:49 -0500

    half homapage done

---
+++ metro/src/components/Homepage/MenuBottom.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import SortIcon from '@mui/icons-material/Sort';
+
+const MenuBottom = () => {
+  return (
+    <>
+    <div className='mt-7 flex flex-wrap'>
+          <div>
+            {/* <img src={Menubar} alt="" className='text-black'/> */}
+            <button className='pl-6 pr-[110px] pt-3 pb-3 bg-black text-white text-2xl rounded-t'><SortIcon fontSize='large' className='mr-2'/>CATEGORIES</button>
+          </div>
+          <div>
+            <input type="text" name="" id="" placeholder='Search Products Here' className='pl-6 pr-[750px] pt-4 pb-4 ml-4 rounded border-2 font-semibold border-solid border-[#ddd] relative outline-0' />
+            </div>           
+            <span className='w-[1px] h-8 bg-[#ddd] absolute ml-[950px] mt-3 '></span>                    
+            <div className='absolute ml-[980px] mt-4'>           
+            <select name="" id="" className='pl-4 pr-8 text-lg outline-0 text-slate-600'>             
+               <option value="">All categories</option>
+               <option value="">Air Filter</option>
+               <option value="">Body Engine</option>
+               <option value="">Brake Part</option>
+               <option value="">Cosmetics</option>
+               <option value="">Covid-19</option>
+               <option value="">Electronics</option>
+               <option value="">Frame Sunglasses</option>
+               <option value="">Furniture</option>
+               <option value="">Genuine Leather</option>
+               <option value="">Gift Corner</option>
+               <option value="">Jewellery</option> 
+               <option value="">Kid's wear</option>
+               <option value="">Medical</option>
+               <option value="">Medical Accessories</option>
+               <option value="">Men</option>
+               <option value="">Modern Furniture</option>
+               <option value="">Other Medical</option>
+            </select>
+            </div>
+            <div>
+              <button className='pl-5 pr-5 pt-3 pb-3 bg-black absolute text-white ml-[-75px] rounded-r'><SearchIcon fontSize='large'/></button>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default MenuBottom
---
+++ metro/src/components/Homepage/MenuSec.jsx
@@ -0,0 +1,67 @@
+// import React from 'react';
+// import darkLogo from "../../assets/";
+// import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+// import SearchIcon from '@mui/icons-material/Search';
+// import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+// import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+// const MenuSec = () => {
+//   return (
+//     <>
+//      <div className='flex flex-wrap justify-between pt-5 overflow-hidden'>
+//           <div className=''>
+//             <img src={darkLogo} alt="" className='w-[130px]'/>
+//           </div>
+//           <div>
+//             <div className='ml-[106px]'>
+//               <ul className='flex flex-wrap space-x-2 text-medium font-medium  mt-3'>
+//                 <li><a href="#">Home<KeyboardArrowDownIcon fontSize='small'/></a></li>
+//                 <li><a href="#">Shop<KeyboardArrowDownIcon fontSize='small'/></a></li>
+//                 <li><a href="#">Card<KeyboardArrowDownIcon fontSize='small'/></a></li>
+//                 <li><a href="#">Checkout<KeyboardArrowDownIcon fontSize='small'/></a></li>
+//                 <li><a href="#">Error<KeyboardArrowDownIcon fontSize='small'/></a></li>
+//                 <li><a href="#">My Account<KeyboardArrowDownIcon fontSize='small'/></a></li>
+//                 <li><a href="#">Contact</a></li>
+//               </ul>
+//             </div>
+//             </div>
+//             <div className='flex flex-wrap space-x-3 mt-2 ml-28'>
+//             <SearchIcon fontSize='large'/>
+//             <PersonOutlineIcon fontSize='large'/>
+//             <FavoriteBorderIcon fontSize='large'/>            
+//             <ShoppingCartIcon fontSize='large'/>
+//             <p className='text-lg mt-2'>$0.00</p>
+//             </div>         
+//         </div>
+//     </>
+//   )
+// }
+
+// export default MenuSec
+
+{/* <div className='flex flex-wrap space-x-44 absolute top-60'>
+        <div className='mt-10'>
+          <h2 className='text-2xl font-normal'>SOUNDBOX</h2>
+          <h1 className='text-6xl font-bold mt-4'>Bluetooth Speaker</h1>
+          <p className='mt-6 text-lg w-[430px]'>New Modern Stylist Fashionable Men's Wearholder
+              bag maxcan weather holder.</p>
+            <h2 className='text-lg mt-6 font-medium'>EXPLORE NOW</h2>
+
+        </div>
+        <div>
+            <img src={slideImgOne} alt="" className='w-[400px]'/>
+        </div>
+        </div> */}
+
+
+        // <Carousel responsive={responsive}
+        // draggable={false}
+        // showDots={true}
+        // ssr={true} // means to render carousel on server-side.
+        // infinite={true}
+        // autoPlay={true }
+        // autoPlaySpeed={1000}
+        // // customTransition="all .5"
+        // animateOut= 'fadeOut'
+        // transitionDuration={500}>
